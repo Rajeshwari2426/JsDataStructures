c4a3a4f35cd39d8c527631cc0cc2b9339aefd9b4
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-03-13 22:33:53 +0530

    [Rajeshwari] adding Repeated digits

--- Arrays/LargestAndSmallestUsingSort.js
+++ Arrays/LargestAndSmallestUsingSort.js
@@ -2,7 +2,7 @@
 let n = new Array(10);
 for (let i = 0; i < n.length; i++) {
   n[i] = Math.floor(Math.random() * 999) ;
-}
+  }
 let sortedArray = n.sort();
 console.log(sortedArray);
 console.log("2nd largest element: " + sortedArray[(sortedArray.length - 2)]);
